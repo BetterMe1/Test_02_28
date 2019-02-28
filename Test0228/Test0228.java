4d3d48e82f41660abf007bc5d0a8752605a6bbfd
BetterMe1 <1754217281@qq.com>
2019-02-28 23:18:54 +0800

    提交代码

---
+++ Test0228/Test0228.java
@@ -0,0 +1,57 @@
+package Test0228;
+
+/*
+470. 用 Rand7() 实现 Rand10()
+已有方法 rand7 可生成 1 到 7 范围内的均匀随机整数，试写一个方法 rand10 生成 1 到 10 范围内的均匀随机整数。
+不要使用系统的 Math.random() 方法。
+
+示例 1:
+输入: 1
+输出: [7]
+示例 2:
+输入: 2
+输出: [8,4]
+示例 3:
+输入: 3
+输出: [8,1,10]
+ 
+提示:
+rand7 已定义。
+传入参数: n 表示 rand10 的调用次数。
+进阶:
+rand7()调用次数的 期望值 是多少 ?
+你能否尽量少调用 rand7() ?
+ */
+public class Test0228 {
+	public static void main(String[] args) {
+		System.out.println(1);
+		
+	}
+}
+/**
+ * The rand7() API is already defined in the parent class SolBase.
+ * public int rand7();
+ * @return a random integer in the range 1 to 7
+ */
+//class Solution extends SolBase {
+//    public int rand10() {
+//        int a = rand7();
+//        int b = rand7();
+//        while(true){
+//        	if(a<=4)
+//        		return b;
+//        	else if(b<=4)
+//        		return a+3;
+//        	else{
+//        		a = rand7();
+//        		b = rand7();
+//        	}
+//        }
+//    }
+//}
+//public class Test0228 {
+//	public static void main(String[] args) {
+//		Solution So = new Solution();
+//		
+//	}
+//}
